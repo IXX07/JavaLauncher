f4ae1dc0448cdf9567fa1a9a906964654bb1e28a
IX07 <108982289+IXX07@users.noreply.github.com>
2023-01-30 17:24:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace JavaLauncher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Java Launcher";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            if (args.Length > 0 && File.Exists(args[0]) && Path.GetExtension(args[0]) == ".jar")
+                Process.Start("CMD.exe", "/C java -jar " + Path.GetFileName(args[0]));
+            else
+            {
+                Console.WriteLine("An unexpected error has occured.");
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
